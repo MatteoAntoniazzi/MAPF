62a42f56ee9edc4d7d61e3e7837bc79a79cc33fa
Matteo <matteo.antoniazzi@mail.polimi.it>
2020-04-10 11:18:39 +0200

    Time out almost done and some fixes in the simulation movements

--- Experiments/single_instance_test.py
+++ Experiments/single_instance_test.py
@@ -25,10 +25,10 @@ problem_map = load_map_from_file(map_path)
 
 #problem_map = generate_random_map(8, 8, 0)
 
-#problem_agents = [Agent(0, (0, 0), (2, 1)), Agent(1, (3, 0), (1, 1))]
+problem_agents = [Agent(0, (0, 0), (2, 1)), Agent(1, (3, 0), (1, 1))]
 # problem_agents = [Agent(0, (6, 6), (0, 4)), Agent(1, (2, 0), (4, 6)), Agent(2, (3, 6), (4, 3)), Agent(3, (0, 2), (7, 4))]
-problem_agents = [Agent(0, (6, 6), (0, 4)), Agent(1, (2, 0), (4, 6)), Agent(2, (3, 6), (4, 3)), Agent(3, (0, 2), (7, 4)),
-                  Agent(4, (6, 4), (0, 7)), Agent(5, (2, 1), (4, 7)), Agent(6, (3, 7), (4, 1)), Agent(7, (0, 0), (7, 5))]
+#problem_agents = [Agent(0, (6, 6), (0, 4)), Agent(1, (2, 0), (4, 6)), Agent(2, (3, 6), (4, 3)), Agent(3, (0, 2), (7, 4)),
+#                  Agent(4, (6, 4), (0, 7)), Agent(5, (2, 1), (4, 7)), Agent(6, (3, 7), (4, 1)), Agent(7, (0, 0), (7, 5))]
 
 # problem_agents = [Agent(0, (1, 2), (1, 2)), Agent(1, (1, 1), (0, 2)), Agent(2, (0, 2), (0, 1))]
 
@@ -37,8 +37,8 @@ problem_instance = ProblemInstance(problem_map, problem_agents)
 #problem_instance = generate_problem_m_star_slides()
 
 solver_settings = SolverSettings(heuristic="AbstractDistance", objective_function="SOC", stay_at_goal=True,
-                                 goal_occupation_time=1, edge_conflict=False, time_out=100)
-solver = IDFramework("A*", solver_settings)
+                                 goal_occupation_time=1, edge_conflict=True, time_out=100)
+solver = IDFramework("Conflict Based Search", solver_settings)
 #solver = AStarSolver(solver_settings)
 
 paths = solver.solve(problem_instance, verbose=True)
--- GUI/start_simulation.py
+++ GUI/start_simulation.py
@@ -2,12 +2,12 @@ from .Visualize import Visualize
 from MAPFSolver import *
 
 
-def prepare_simulation(reader, frame, algorithm, independence_detection, solver_settings, n_of_agents):
+def prepare_simulation(reader, frame, algorithm_str, independence_detection, solver_settings, n_of_agents):
     """
     Solve the MAPF problem and visualize the simulation on the frame.
     :param reader: Reader object for the loading of the map and the scen
     :param frame: Frame where the simulation will be displayed
-    :param algorithm: String of the algorithm choose
+    :param algorithm_str: String of the algorithm choose
     :param independence_detection: True if Independence Detection will be used with the algorithm
     :param solver_settings: Settings of the solver (heuristics, goal_occupation_time)
     :param n_of_agents: Number of Agents on the map
@@ -16,7 +16,10 @@ def prepare_simulation(reader, frame, algorithm, independence_detection, solver_
     agents = load_agents(reader, problem_map, n_of_agents)
     problem_instance = ProblemInstance(problem_map, agents)
 
-    solver = get_solver(algorithm, solver_settings, independence_detection)
+    if independence_detection:
+        solver = IDFramework(algorithm_str, solver_settings)
+    else:
+        solver = get_solver(algorithm_str, solver_settings)
     print("Solver --> ", solver, "\nSolving...")
     paths, output_infos = solver.solve(problem_instance, verbose=True, return_infos=True)
     print("Solved.")
@@ -46,23 +49,5 @@ def plot_on_gui(problem_instance, solver_settings, frame, paths=None, output_inf
     """
     window = Visualize(problem_instance, solver_settings, frame, paths, output_infos)
     window.initialize_window()
-
-
-def get_solver(algorithm, solver_settings, independence_detection):
-    """
-    Return the Solver object for the specified algorithm and relative settings.
-    """
-    switcher = {
-        "Cooperative A*": CooperativeAStarSolver(solver_settings),
-        "A*": AStarSolver(solver_settings),
-        "A* with Operator Decomposition": AStarODSolver(solver_settings),
-        "Increasing Cost Tree Search": ICTSSolver(solver_settings),
-        "Conflict Based Search": CBSSolver(solver_settings),
-        "M*": MStarSolver(solver_settings)
-    }
-    if independence_detection:
-        return IDFramework(switcher.get(algorithm), solver_settings)
-    else:
-        return switcher.get(algorithm)
 
 
